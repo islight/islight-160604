6d459856b17f21b1df9407fd87d1c6495376272c
lingguang.ylg <lingguang.ylg@alibaba-inc.com>
2017-03-07 14:22:36 +0800

    error watch dog

--- errorwatchdog.py
+++ errorwatchdog.py
@@ -1,16 +1,32 @@
 # coding=gbk
+import os
 import time
 import requests
 
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
 
-class WatchDog(object):
+
+class WatchDog(FileSystemEventHandler):
     def __init__(self, filename):
+        self.filename = filename;
         self.file = open(filename, 'r')
         l = self.file.readline()
         while l:
             l = self.file.readline()
 
+    def on_created(self, event):
+        super(WatchDog, self).on_created(event)
+
+        if self.filename == event.src_path:
+            self.file = open(self.filename, 'r')
+            print 'reload '+ self.filename
+
     def watch(self):
+        observer = Observer()
+        observer.schedule(self, os.path.dirname(self.filename), recursive=False)
+        observer.start()
+
         while True:
             l = self.file.readline()
             if not l:
@@ -26,6 +42,5 @@ class WatchDog(object):
             json={"msgtype": "text", "text": {"content": msg.decode('gb2312')}})
         print res.text
 
-
-w = WatchDog('/home/admin/logs/certdoccenter/certdoccenter-biz.log')
-w.watch()
+wd = WatchDog('/tmp/a.log')
+wd.watch()
